cc240c087b4b48f4c6fd1064f6a755e9635070c1
Jussi Toivonen <jupito@iki.fi>
2017-08-06 23:18:18 +0300

    misc

--- podxm.sh
+++ podxm.sh
@@ -1,55 +1,53 @@
 #!/bin/ksh
 
-prg="podxm"
+# Run podxm. Ksh is used because sh/dash doesn't support "!(music()".
 
-param=$1
-test $param || param=default
+do_param() {
+    param="$1"
+    set -x
+    case "$param" in
+    ui)
+        podxm -c ui -w ,D,1,SD -d !(music*)
+        ;;
+    ui-m)
+        podxm -c ui -w ,d,1,Sd -d music*
+        ;;
+    ui-iot)
+        podxm -c ui -w foin,d,-1,d -d 'misc,dl=0/In Our Time'*
+        ;;
+    ref)
+        parallel-moreutils -j10 -n10 podxm -c refresh -d -- */*
+        ;;
+    dl)
+        podxm -c dl -w in,D,3,SD -d !(music*)
+        podxm -c norm -w in,D,3,SD -d !(music*)
+        ;;
+    dl-m)
+        podxm -c dl -w in,d,5,Sd -d music*
+        podxm -c norm -w in,d,5,Sd -d music*
+        ;;
+    dl-ia)
+        podxm -c dl -w ia,,-1,SD --force -d *
+        podxm -c norm -w foina,,-1,SD -d *
+        ;;
+    src)
+        $VISUAL $0
+        exit
+        ;;
+    *)
+        echo "Unknown parameter: $1"
+        exit 1
+        ;;
+    esac
+}
 
-case "$param" in
-ui|default)
-    cmd="$prg -c ui -w ,D,1,SD -d !(music*)"
-    ;;
-ui-music)
-    cmd="$prg -c ui -w ,d,1,Sd -d music*"
-    ;;
-ui-iot)
-    cmd="$prg -c ui -w foin,d,-1,d -d 'misc,dl=0/In Our Time'*"
-    ;;
-ref)
-    cmd="parallel-moreutils -j10 -n10 $prg -c refresh -d -- */*"
-    ;;
-dl)
-    # # $prg -c dl -w fn,fpD,3, -d !(music*)
-    # $prg -c dl -w foina,D,3, -d !(music*)
-    # $prg -c norm -w foina,,-1, -d !(music*)
-    dir="!(music*)"
-    view="oina,D,3,"
-    cmd1="$prg -c dl -w $view -d $dir"
-    cmd2="$prg -c norm -w $view -d $dir"
-    cmd="$cmd1 && $cmd2"
-    ;;
-dl-music)
-    dir="music*"
-    view="n,d,5,"
-    cmd1="$prg -c dl -w $view -d $dir"
-    cmd2="$prg -c norm -w $view -d $dir"
-    cmd="$cmd1 && $cmd2"
-    ;;
-dl-ia)
-    cmd1="$prg -c dl -w ia,SD,-1, --force -d *"
-    cmd2="$prg -c norm -w foina,SD,-1, -d *"
-    cmd="$cmd1 && $cmd2"
-    ;;
-src)
-    $VISUAL $0
-    exit
-    ;;
-*)
-    print "Unknown parameter: $1"
-    exit 1
-    ;;
-esac
+set -e
+#typeset -t do_param
 
 cd $HOME/podcasts
-print "Running: $cmd"
-eval "$cmd"
+
+for param; do
+    echo
+    echo $param
+    do_param $param
+done
