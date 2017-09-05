f44f14d25d425d0d60cfcb81010c4fba8445100c
Jussi Toivonen <jupito@iki.fi>
2017-09-05 23:52:30 +0300

    misc

--- podxm.sh
+++ podxm.sh
@@ -17,38 +17,48 @@ _nice() {
     nice ionice -c3 "$@"
 }
 
+_du() {
+    size="$(du -hs "$1")"
+    nfiles="$(find "$1" -type f | wc -l)"
+    echo "$nfiles files, "$size""
+}
+
 _sync() {
     # Sync files with Jolla.
     tmp="$(_tmpfile)"
-    nfiles=15
+    n=15
     prefix="$1"
     dst="$HOME/jolla/Music/podcasts/$prefix"
 
     #dirs="$(echo $HOME/podcasts/!(complete*|done*|music*|video*))"
     #dirs="$(echo $HOME/podcasts/!(complete*|done*|video*))"
     dirs="$(echo $HOME/podcasts/$prefix*)"
-    cmd="podxm -c show_files -w in,1,SD, -d $dirs"
+    cmd="podxm -c show_files -w oin,1,SD, -d $dirs"
 
-    $cmd | grep '\.\(mp3\|ogg\)' | head -n $nfiles > "$tmp"
-    echo "Prefix: $prefix, files: $(wc -l < $tmp), size: $(xargs du -ch < $tmp | tail -n 1)"
+    $cmd | grep '\.\(mp3\|ogg\)' | head -n $n > "$tmp"
+
+    # nfiles="$(wc -l < $tmp)"
+    # size="$(xargs du -chs < $tmp | tail -n 1)"
+    # echo "$prefix: $nfiles files, "$size""
 
     #less "$tmp"
     mkdir -p "$dst"
     rm -rf "$dst"/*
     xargs ln -f -t "$dst" < "$tmp"
-    du -h "$dst"
     rm "$tmp"
+    _du "$dst"
 }
 
 do_param() {
     case "$1" in
     ui)
-        # podxm -c ui -w ,1,D,SD -d !(music*)
         podxm -c ui -w ,1,D,SD -d $dirs_nomusic
         ;;
     ui-m)
-        # podxm -c ui -w ,1,d,Sd -d music*
-        podxm -c ui -w ,1,d,Sd -d $dirs_music
+        podxm -c ui -w ,1,Sd,Sd -d $dirs_music
+        ;;
+    ui-f)
+        podxm -c ui -w f,-1,D,SD -d *
         ;;
     ui-iot)
         podxm -c ui -w foin,-1,d,d -d 'misc,dl=0/In Our Time'*
@@ -60,7 +70,7 @@ do_param() {
         find -mindepth 2 -type d -print0 | xargs -0 -L10 -P10 podxm -c refresh -d
         ;;
     dl)
-        _nice podxm -c dl norm -w in,3,D,SD -d $dirs_nomusic
+        _nice podxm -c dl norm -w n,3,D,SD -d $dirs_nomusic
         ;;
     dl-m)
         _nice podxm -c dl norm -w n,3,d,Sd -d $dirs_music
@@ -72,16 +82,17 @@ do_param() {
         _nice podxm -c norm -w foina,-1,,SD -d *
         ;;
     sync)
+        rm -rf ~/jolla/Music/playlists
+        cp -a ~/Music/playlists ~/jolla/Music
         _sync 0current
         _sync complete
         _sync history
         _sync misc
         _sync music
-        du -hcs "$HOME/jolla/Music/podcasts"
+        _du ~/jolla/Music/podcasts
         ;;
     src)
-        $VISUAL $0
-        exit
+        $VISUAL "$0"
         ;;
     *)
         echo "Unknown parameter: $1"
